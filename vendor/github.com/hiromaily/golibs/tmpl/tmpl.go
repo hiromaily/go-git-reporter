54d27f71a8079f80d1ec129beb92d9304667963d
hiromaily <hiromaily2@gmail.com>
2017-07-15 17:47:53 +0200

    added godep files

---
+++ vendor/github.com/hiromaily/golibs/tmpl/tmpl.go
@@ -0,0 +1,46 @@
+package tmpl
+
+import (
+	"bytes"
+	//ht "html/template"
+	tt "text/template"
+)
+
+// StrTempParser is to get string of template for string
+// It's just example
+func StrTempParser(temp string, params interface{}) (string, error) {
+	var parseResult bytes.Buffer
+
+	funcMap := tt.FuncMap{
+		"add": func(a, b int) int { return a + b },
+	}
+
+	tpl := tt.Must(tt.New("tpl").Funcs(funcMap).Parse(temp))
+
+	if err := tpl.Execute(&parseResult, params); err != nil {
+		return "", err
+	}
+
+	return parseResult.String(), nil
+}
+
+// FilePathParser is to get string from template file
+func FilePathParser(path string, params interface{}) (string, error) {
+	var parseResult bytes.Buffer
+
+	tpl := tt.Must(tt.ParseFiles(path))
+	if err := tpl.Execute(&parseResult, params); err != nil {
+		return "", err
+	}
+	return parseResult.String(), nil
+}
+
+// FileTempParser is to get string from template.Template
+func FileTempParser(tpl *tt.Template, params interface{}) (string, error) {
+	var parseResult bytes.Buffer
+
+	if err := tpl.Execute(&parseResult, params); err != nil {
+		return "", err
+	}
+	return parseResult.String(), nil
+}
